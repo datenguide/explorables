f9c922cd1532b42679d81362f2ad2e0f647e473c
P3nny <p3nny@hacking.studio>
2020-04-20 15:39:33 +0200

    🚧 Add mapbox gl component

---
+++ components/map.js
@@ -0,0 +1,29 @@
+import React, {
+    useState
+} from 'react';
+import ReactMapGL from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+function Map() {
+    const [viewport, setViewport] = useState({
+        width: 400,
+        height: 400,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8,
+        mapboxApiAccessToken: '',
+    });
+
+    return ( <
+        ReactMapGL {
+            ...viewport
+        }
+        onViewportChange = {
+            setViewport
+        }
+        />
+    );
+}
+
+export default Map;
---
+++ stories/2-Map.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    action
+} from '@storybook/addon-actions';
+import Map from '../components/map';
+
+export default {
+    title: 'Map',
+    component: Map,
+};
+
+export const map = () => ( <
+    Map / >
+);
