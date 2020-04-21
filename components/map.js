c3b865789180749b8a5e550ce92e419384f99d4f
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-04-21 10:58:19 +0200

    Render layer from geojson

--- components/map.js
+++ components/map.js
@@ -1,19 +1,47 @@
-import React, { useState } from 'react'
-import ReactMapGL from 'react-map-gl'
+import React, { useState, useEffect } from 'react'
+import { StaticMap, Source, Layer } from 'react-map-gl'
+import { json } from 'd3-request'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 
+const regionLayer = {
+  type: 'fill',
+  source: 'geojson',
+  paint: {
+    'fill-color': '#ff0000',
+    'fill-opacity': 0.4,
+  },
+}
+
 function Map({ mapboxApiAccessToken }) {
   const [viewport, setViewport] = useState({
-    width: 400,
+    width: '100%',
     height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+    latitude: 51.427,
+    longitude: 7.664,
+    zoom: 6,
+    options: false,
     mapboxApiAccessToken,
   })
 
-  return <ReactMapGL {...viewport} onViewportChange={setViewport} />
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    // code to run on component mount
+    json('/nrw_bundesland.geojson', (error, response) => {
+      if (!error) {
+        setData(response)
+      }
+    })
+  }, [])
+
+  return (
+    <StaticMap {...viewport} onViewportChange={setViewport}>
+      <Source type="geojson" data={data}>
+        <Layer {...regionLayer} />
+      </Source>
+    </StaticMap>
+  )
 }
 
 export default Map
