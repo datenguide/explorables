f48e3f11c2c82cf750f99c93fee6a3f722dbcd20
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-04-22 10:01:02 +0200

    Add support for SASS/SCSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,16 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-};
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    })
+
+    // Return the altered config
+    return config
+  },
+}
