90d412b4b31fa6905b757d792a6e4e2e39d12831
Christian Rijke <hello@christianrijke.de>
2020-04-24 13:18:41 +0200

    Transform material ui theme to scss.

--- components/RegionKeys/index.js
+++ components/RegionKeys/index.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 
-import theme from '../muiTheme'
+import theme from '../../lib/muiTheme'
 import RegionSelect from './RegionSelect'
 import Grid from '@material-ui/core/Grid'
 import RegionCards from './RegionCards'
+
+// import styles from './styles.scss'
 
 const RegionKeys = () => {
   const [region, setRegion] = useState(null)
@@ -26,6 +28,7 @@ const RegionKeys = () => {
         </Grid>
         <Grid item xs={12}>
           <RegionCards region={region} />
+          <h1>123 123 123 123</h1>
         </Grid>
       </Grid>
     </ThemeProvider>
