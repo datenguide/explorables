435ec75c001b629263304cb281d92b264bddaf3e
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-04-30 20:02:15 +0200

    Add generic wrapper component for static maps

--- components/index.js
+++ components/index.js
@@ -3,6 +3,7 @@ import MapTooltip from './MapTooltip'
 import MapNav from './MapNav'
 import ShapeLayer from './ShapeLayer'
 import Map from './Map'
+import StaticMap from './StaticMap'
 import SimpleMap from './SimpleMap'
 
-export { Button, MapTooltip, MapNav, ShapeLayer, Map, SimpleMap }
+export { Button, MapTooltip, MapNav, ShapeLayer, Map, StaticMap, SimpleMap }
