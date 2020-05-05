60eea1e9d159a4d8b76280cbaaacde1f74847ae6
Simon Jockers <449739+sjockers@users.noreply.github.com>
2020-05-05 19:15:35 +0200

    Replace StaticMap component with generic, configurable map component

--- components/Map.js
+++ components/Map.js
@@ -1,101 +1,24 @@
 import React, { useState } from 'react'
-import { StaticMap } from 'react-map-gl'
-import MapNav from './MapNav'
-import MapTooltip from './MapTooltip'
-import ShapeLayer from './ShapeLayer'
+import ReactMapGl from 'react-map-gl'
 
-import 'mapbox-gl/dist/mapbox-gl.css'
-
-const paths = {
-  nuts1: 'bundeslaender.json',
-  nuts2: 'nrw_regierungsbezirke.json',
-  nuts3: 'nrw_landkreise.json',
-  lau: 'nrw_gemeinden.json',
-}
-
-const layerOptions = {
-  nrw: {
-    filter: ['==', 'name', 'Nordrhein-Westfalen'],
-    paint: {
-      'fill-color': '#ff0000',
-      'fill-opacity': 0.4,
-      'fill-outline-color': '#ffffff',
-    },
-  },
-  states: {
-    filter: ['!=', 'name', 'Nordrhein-Westfalen'],
-    paint: {
-      'fill-color': '#0000ff',
-      'fill-opacity': 0.2,
-      'fill-outline-color': '#ffffff',
-    },
-  },
-}
-
-const mapNavItems = [
-  {
-    id: 'nuts1',
-    title: 'Bundesländer',
-  },
-  {
-    id: 'nuts2',
-    title: 'Statistische Regionen',
-  },
-  {
-    id: 'nuts3',
-    title: 'Landkreise',
-  },
-  {
-    id: 'lau',
-    title: 'Gemeinden',
-  },
-]
-
-function Map({ mapboxApiAccessToken, rootPath }) {
-  const [level, setlevel] = useState(mapNavItems[0])
-  const [viewport, setViewport] = useState({
-    width: '100%',
-    height: 400,
+function Map({ children, settings, viewport }) {
+  const [viewportConfig, setViewportConfig] = useState({
+    width: 400,
+    height: 300,
     latitude: 51.427,
     longitude: 7.664,
     zoom: 6,
-    options: false,
-    mapboxApiAccessToken,
+    ...viewport,
   })
 
   return (
-    <StaticMap {...viewport} onViewportChange={setViewport}>
-      <MapTooltip lonLat={[7.405, 51.509]}>You are here!</MapTooltip>
-      <MapNav
-        items={mapNavItems}
-        currentItem={level}
-        onItemClick={(item) => {
-          setlevel(item)
-        }}
-      />
-      <ShapeLayer
-        src={`${rootPath}/${paths.nuts1}`}
-        options={layerOptions.nrw}
-        hidden={level.id !== 'nuts1'}
-      />
-      <ShapeLayer
-        src={`${rootPath}/${paths.nuts1}`}
-        options={layerOptions.states}
-        hidden={level.id !== 'nuts1'}
-      />
-      <ShapeLayer
-        src={`${rootPath}/${paths.nuts2}`}
-        hidden={level.id !== 'nuts2'}
-      />
-      <ShapeLayer
-        src={`${rootPath}/${paths.nuts3}`}
-        hidden={level.id !== 'nuts3'}
-      />
-      <ShapeLayer
-        src={`${rootPath}/${paths.lau}`}
-        hidden={level.id !== 'lau'}
-      />
-    </StaticMap>
+    <ReactMapGl
+      {...viewportConfig}
+      {...settings}
+      onViewportChange={setViewportConfig}
+    >
+      {children}
+    </ReactMapGl>
   )
 }
 
--- components/index.js
+++ components/index.js
@@ -3,7 +3,6 @@ import MapTooltip from './MapTooltip'
 import MapNav from './MapNav'
 import ShapeLayer from './ShapeLayer'
 import Map from './Map'
-import StaticMap from './StaticMap'
 import SimpleMap from './SimpleMap'
 
-export { Button, MapTooltip, MapNav, ShapeLayer, Map, StaticMap, SimpleMap }
+export { Button, MapTooltip, MapNav, ShapeLayer, Map, SimpleMap }
